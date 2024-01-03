8d166ca6fa0a38a03a3bc385113ed7ca27529f6c
Jack Himes <jack.himes3246@gmail.com>
2024-01-03 14:28:09 -0700

    Install and configure nextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import {Button, ButtonGroup} from "@nextui-org/react";
 
 import Header from './components/Header'
 
 export default function Home() {
   return (
     <div>
+      <Button color='primary'>Click me</Button>
       <Header />
       <div>Hello World</div>
       </div>
