c94e5defb44978268379c06289e81e479275a47a
Jack Himes <jack.himes3246@gmail.com>
2024-01-14 15:45:57 -0700

    style sign in page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,34 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp />;
+  return (
+      <div className="flex items-center justify-center h-screen bg-green-200">
+        <SignUp
+            appearance={{
+                elements: {
+                    formButtonPrimary:
+                        "bg-yellow-500 hover:bg-yellow-400 text-sm normal-case",
+                    card:
+                        "bg-emerald-800",
+                    headerTitle:
+                        "text-amber-50",
+                    headerSubtitle:
+                        "text-amber-50",
+                    formFieldLabel:
+                        "text-amber-50",
+                    footerActionText:
+                        "text-amber-50",
+                    footerActionLink:
+                        "text-blue-400",
+                    socialButtonsBlockButton__google:
+                        "bg-slate-50 hover:bg-slate-200",
+                    dividerLine:
+                        "bg-amber-50",
+                    dividerText:
+                        "text-amber-50",
+                },
+            }} 
+        />
+    </div>
+    );
 }
