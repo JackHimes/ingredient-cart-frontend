3d45d80296c51e6c8e8b934e402b46063f87a0a8
Jack Himes <jack.himes3246@gmail.com>
2024-04-18 13:27:53 -0600

    Update Navigation links

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -12,18 +12,18 @@ export default function Navigation() {
             </NavbarBrand>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
+          <Link color="foreground" href="../">
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
+          <Link color="foreground" href="../ingest" aria-current="page">
+            Automated Cart
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link color="foreground" href="../configuration">
+            Settings
           </Link>
         </NavbarItem>
       </NavbarContent>
