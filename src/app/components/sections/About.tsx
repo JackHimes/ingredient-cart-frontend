4195d073f14c1bda42dd94e9ba49e268ad54b479
Jack Himes <jack.himes3246@gmail.com>
2024-04-29 15:19:45 -0600

    Reorganize About section layout

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -5,32 +5,34 @@ export default function About() {
   return (
     <div className="bg-off-white py-12">
       <div className="mx-36 flex justify-between">
-        <h1 className="w-80 text-dark-green text-4xl font-sans">
-          Save Time and Money When Cooking
-        </h1>
-        <p className="w-96 text-dark-green font-sans">
-          Ingredient Cart is your gateway to a virtual culinary experience. With
-          just a click, you can explore an array of recipes and ingredients
-          without leaving your home. It's an easy and convenient way to discover
-          new flavors and create delightful meals.
-        </p>
-      </div>
-      <div className="mx-36 flex justify-between">
-        <Image
-          className="mt-44"
-          src="/about-section-cooking.jpg"
-          alt="Cooking"
-          width={416}
-          height={515}
-        />
-        <Image
-          className="mt-64"
-          src="/about-section-bagged-food.jpg"
-          alt="Bagged Food"
-          width={335}
-          height={400}
-          style={{ width: "335px", height: "400px" }}
-        />
+        <div className="flex flex-col">
+          <h1 className="w-80 text-dark-green text-4xl font-sans">
+            Save Time and Money When Cooking
+          </h1>
+          <Image
+            className="mt-44"
+            src="/about-section-cooking.jpg"
+            alt="Cooking"
+            width={416}
+            height={515}
+          />
+        </div>
+        <div className="flex flex-col">
+          <p className="w-96 text-dark-green font-sans">
+            Ingredient Cart is your gateway to a virtual culinary experience.
+            With just a click, you can explore an array of recipes and
+            ingredients without leaving your home. It's an easy and convenient
+            way to discover new flavors and create delightful meals.
+          </p>
+          <Image
+            className="mt-56"
+            src="/about-section-bagged-food.jpg"
+            alt="Bagged Food"
+            width={335}
+            height={400}
+            style={{ width: "335px", height: "400px" }}
+          />
+        </div>
       </div>
       <div className="mx-36 pt-44 flex justify-between">
         <div className="flex flex-col items-center">
