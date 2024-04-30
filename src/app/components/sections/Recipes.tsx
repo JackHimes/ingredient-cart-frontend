85003286b98069a0693d4f2768a2f79cad69257f
Jack Himes <jack.himes3246@gmail.com>
2024-04-30 15:54:14 -0600

    add button

--- src/app/components/sections/Recipes.tsx
+++ src/app/components/sections/Recipes.tsx
@@ -1,17 +1,22 @@
 "use client";
-import Image from 'next/image';
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function Recipes() {
   return (
     <div className="p-10">
       <div className="flex flex-col items-center">
         <div className="py-16">
-          <h1 className="text-2xl text-green-text font-bold text-center">Recommended Recipes</h1>
+          <h1 className="text-2xl text-green-text font-bold text-center">
+            Recommended Recipes
+          </h1>
         </div>
         <div className="flex justify-between space-x-48">
           <div className="flex flex-col items-start p-4">
             <Image src="/pizza.jpg" alt="Recipe 2" width={287} height={200} />
-            <h1 className="text-xl text-green-text font-semibold my-8">Pizza Margherita</h1>
+            <h1 className="text-xl text-green-text font-semibold my-8">
+              Pizza Margherita
+            </h1>
             <p className="text-green-text w-72">
               This pizza is simple, fresh and a guaranteed crowd pleaser. Its a
               delight to make and even better to eat. How about pizza tonight?
@@ -24,7 +29,9 @@ export default function Recipes() {
               width={287}
               height={200}
             />
-            <h1 className="text-xl text-green-text font-semibold my-8">General Tso's Chicken</h1>
+            <h1 className="text-xl text-green-text font-semibold my-8">
+              General Tso's Chicken
+            </h1>
             <p className="text-green-text w-72">
               Looking for tangy, spicy, and savory flavors all found in one
               dish? This top-rated recipe for General Tso's chicken combines
@@ -34,7 +41,9 @@ export default function Recipes() {
           </div>
           <div className="flex flex-col items-start p-4">
             <Image src="/salad.jpg" alt="Recipe 3" width={287} height={200} />
-            <h1 className="text-xl text-green-text font-semibold my-8">Caesar Salad</h1>
+            <h1 className="text-xl text-green-text font-semibold my-8">
+              Caesar Salad
+            </h1>
             <p className="text-green-text w-72">
               A great Caesar salad hits a satisfying balance between fresh and
               crisp, and creamy and cheesy. Its the perfect accompaniment to a
@@ -42,6 +51,14 @@ export default function Recipes() {
             </p>
           </div>
         </div>
+        <Button
+          variant="bordered"
+          radius="none"
+          size="lg"
+          className="mt-14 mb-36 w-72 border-dark-green bg-peach font-sans"
+        >
+          Search Recipes
+        </Button>
       </div>
     </div>
   );
