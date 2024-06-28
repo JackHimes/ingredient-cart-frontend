6aaa5e4d66a4e3a66ea50bbbc2e1c2bed7e5a08f
Jack Himes <jack.himes3246@gmail.com>
2024-06-28 14:39:24 -0600

    add auto detect for new recipes from url, made most fields optional for non url recipes, add new recipe attributes, abstract ingest logic to file

--- src/app/types/Recipe.tsx
+++ src/app/types/Recipe.tsx
@@ -1,9 +1,17 @@
 export interface Recipe {
-    _id: string;
-    title: string;
-    ingredients: string[];
-    instructions: string[];
-    url: string;
-    image: string;
-    description: string;
-  }
+  _id: string;
+  title: string;
+  ingredients: string[];
+  instructions: string[];
+  description?: string;
+  url?: string;
+  image?: string;
+  author?: string;
+  category?: string;
+  cuisine?: string;
+  yields?: string;
+  total_time?: number;
+  createdBy: string;
+}
+
+export type RecipeCreationParams = Omit<Recipe, "_id">;
